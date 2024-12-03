a35933315951083e9a9d271195ed50253575d31a
Sergey-2410 <167428697+Sergey-2410@users.noreply.github.com>
2024-12-03 07:08:08 +0200

    add persistore and update css style

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,40 +5,6 @@ import ContactForm from './ContactForm/ContactForm';
 import SearchBox from './SearchBox/SearchBox';
 
 const App = () => {
-  // const data = [
-  //   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  //   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  //   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  //   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  // ];
-  // const [contacts, setContacts] = useState(
-  //   () => JSON.parse(localStorage.getItem('contacts')) ?? data
-  // );
-  // const [value, setValue] = useState('');
-
-  // useEffect(() => {
-  //   localStorage.setItem('contacts', JSON.stringify(contacts));
-  // }, [contacts]);
-
-  // const dataRequest = e => {
-  //   setValue(e.target.value);
-  // };
-
-  // const filterContacts = contacts.filter(contact =>
-  //   contact.name.toLowerCase().includes(value.toLowerCase())
-  // );
-  // const handleDelete = id => {
-  //   setContacts(prev => prev.filter(contact => contact.id !== id));
-  // };
-  // const handleAdd = ({ username, tel }) => {
-  //   const newContact = {
-  //     id: nanoid(),
-  //     name: username,
-  //     number: tel,
-  //   };
-  //   setContacts(prev => [...prev, newContact]);
-  // };
-
   return (
     <div>
       <h1>Phonebook</h1>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,47 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { contactsReducer } from './contactsSlice';
 import { filtersReducer } from './filtersSlice';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  key: 'contacts',
+  version: 1,
+  storage,
+};
+
+const persistConfigFilter = {
+  key: 'filter',
+  version: 1,
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, contactsReducer);
+const persistedReducerFilter = persistReducer(
+  persistConfigFilter,
+  filtersReducer
+);
 
 export const store = configureStore({
   reducer: {
-    contacts: contactsReducer,
-    filters: filtersReducer,
+    contacts: persistedReducer,
+    filters: persistedReducerFilter,
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export let persistor = persistStore(store);
